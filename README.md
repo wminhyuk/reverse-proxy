525eeffb43321ce09d5ee80d8e9353e7e47c3a0e
wminhyuk <seoseo1998@gmail.com>
2025-02-26 14:43:19 +0900

    test

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # reverse-proxy
 - 성능
 - 부하분산(LB)
@@ -8,9 +7,8 @@
 ```bash
 $ python -m http.server --directory pyweb1 8001
 $ python -m http.server --directory pyweb2 8002
-$ python -m http.server --directory blog 8003
+$ python -m http.server 8003 --directory blog 
 ```
-
 ## nginx
 - https://ubuntu.com/tutorials/install-and-configure-nginx#1-overview
 ```bash
@@ -32,11 +30,11 @@ $ pwd
 $ tree -L 2
 .
 ├── ngrinder-agent
-│   ├── lib
-│   ├── run_agent.sh
-│   ├── run_agent_bg.sh
-│   ├── run_agent_internal.sh
-│   └── stop_agent.sh
+│   ├── lib
+│   ├── run_agent.sh
+│   ├── run_agent_bg.sh
+│   ├── run_agent_internal.sh
+│   └── stop_agent.sh
 └── ngrinder-controller
     └── ngrinder-controller-3.5.9-p1.war
 
@@ -56,10 +54,11 @@ $ docker compose start  # 중지된 컨테이너 다시 시작
 $ docker compose restart  # 컨테이너 재시작
 $ docker compose down  # 컨테이너, 네트워크 제거
 $ sudo docker compose stats # 성능 모니터링
-
-# scale out
-$ sudo docker compose up -d --scale web1=3
+$ sudo docker compose up -d --scale web1=3 # scale out
+$ sudo docker compose logs -f api # 로그확인
+$ sudo docker compose up -d --build api # 특정 컨테이너 다시 빌드하여 띄우기
 ```
+
 ## nginx
 ```bash
 $ sudo docker exec -it <LB_NAME> bash
@@ -70,4 +69,17 @@ $ nginx -s reload
 - https://fastapi.tiangolo.com/
 ```bash
 $ fastapi dev main.py
+```
+
+## PDM to requirements.txt
+```bash
+$ pdm export -o requirements.txt --without-hashes
+```
+
+## Docker build & push & pull run
+```bash
+$ sudo docker build -t sawyertom/api:6.1.0 -f docker/fastapi/Dockerfile .
+$ sudo docker login
+$ sudo docker push sawyertom/api:6.1.0
+$ sudo docker run -d --name api610 -p 8610:80 sawyertom/api:6.1.0
 ```
--- main.py
+++ main.py
@@ -5,16 +5,21 @@ import time
 
 app = FastAPI()
 
+# 상수
+N = 10**4  # 100만 개 요소
+N = 10_000
 
 @app.get("/")
 def read_root():
-    a=[1,2,3,4]
-    b=[5,6,7,8]
+    a = [1, 2, 3, 4]
+    b = [5, 6, 7, 8]
+
     result = []
-    # result = [x + y for x, y in zip(a, b)]
-    for i in range(len(a)):
+    for i in range(len(a)): # zip(a, b)
         result.append(a[i] + b[i])
+        
     return {"Hello": result}
+
 
 @app.get("/two-dimensional-array") #a[0][1]
 def two_dimensional_array():
@@ -29,21 +34,13 @@ def two_dimensional_array():
         [6, 5, 4],
         [3, 2, 1]
     ]
-    result = []
-    rows = len(A)       # 행의 길이 (예: 3)
-    cols = len(A[0])    # 열의 길이 (예: 3)
-
-    for i in range(rows):
-        row = []
-        for j in range(cols):
-            row.append(A[i][j] + B[i][j])
-        result.append(row)
-    return {"Hello": result}
+    
+    result = a + b
+    return {"result": result}
 
 @app.get("/add-large-arrays")
 def add_large_arrays():
-    N = 10**6  # 100만 개 요소
-    array_creation_time, addition_time = add_arrays(N, gen_r_array_randint)
+    array_creation_time, addition_time = add_arrays(N, gen_r_array_randint, plus_py)
     return {
         "array_creation_time": array_creation_time,
         "addition_time": addition_time
@@ -51,21 +48,29 @@ def add_large_arrays():
     
 @app.get("/add-large-arrays-choices")
 def add_large_arrays_choices():
-    N = 10**6  # 100만 개 요소
-    array_creation_time, addition_time = add_arrays(N, gen_r_array_choices)
+    array_creation_time, addition_time = add_arrays(N, gen_r_array_choices, plus_py)
     return {
         "array_creation_time": array_creation_time,
         "addition_time": addition_time
         }
-
+    
 def gen_r_array_randint(N):
     a = [random.randint(0, 100) for _ in range(N)]
     b = [random.randint(0, 100) for _ in range(N)]
     return a,b
 
+def gen_r_array_choices(N):
+    a = random.choices(range(101), k=N)
+    b = random.choices(range(101), k=N)
+    return a,b
 
+def plus_py(a, b):
+    result = []
+    for x, y in zip(a, b):
+        result.append(x + y)
+    return result
 
-def add_arrays(N, fun):
+def add_arrays(N, fun, fun_plus):
     # 랜덤한 1차원 배열 2개 생성
     start_creation_time = time.time()
     a, b = fun(N)
@@ -74,9 +79,7 @@ def add_arrays(N, fun):
     # 실행 시간 측정 시작
     add_start_time = time.time()
     # 요소별 덧셈
-    result = []
-    for x, y in zip(a, b):
-        result.append(x + y)
+    result = fun_plus(a, b)
      
     # 실행 시간 측정 종료
     add_end_time = time.time()
@@ -87,11 +90,7 @@ def add_arrays(N, fun):
     addition_time = add_end_time - add_start_time
     return array_creation_time, addition_time
 
-def gen_r_array_choices(N):
-    a = random.choices(range(101), k=N)
-    b = random.choices(range(101), k=N)
-    return a,b
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "qqq": q}
